3cfcb1424d4559e82b4514004b9f21d7945f6ffb
Debby <gaoyuehan23@126.com>
2024-11-18 21:30:55 +0800

    背包

--- Assets/_Scripts/Managers/InputManager.cs
+++ Assets/_Scripts/Managers/InputManager.cs
@@ -2,56 +2,51 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using System;
 
-public class InputManager 
+public class InputManager
 {
-    public static string MOVE="Move";
-    public static string INTERACT="Interact";
-    public static string CURSORLEFT="CursorLeft";
-    public static string CURSORRIGHT="CursorRight";
+    public static string MOVE = "Move";
+    public static string INTERACT = "Interact";
+    public static string CURSORLEFT = "CursorLeft";
+    public static string CURSORRIGHT = "CursorRight";
+    public static string DROP = "Drop"; 
 
-    public void InputValueBinding<T>(string name,Action<bool,T> listener) where T : struct
+    // 用于绑定并监听值类型的输入（比如鼠标坐标或其他结构体）
+    public void InputValueBinding<T>(string name, Action<bool, T> listener) where T : struct
     {
-        var action=InputSystem.actions.FindAction(name);
-        if ( action != null )
+        var action = InputSystem.actions.FindAction(name);
+        if (action != null)
         {
-            //action.started+= (x)=> {
-            //    var data = x.ReadValue<T>();
-            //    listener?.Invoke(true,data);
-            //};
-            action.performed+= (x)=> {
+            action.performed += (x) => {
                 var data = x.ReadValue<T>();
-                listener?.Invoke(true,data);
+                listener?.Invoke(true, data);
             };
 
-            action.canceled += (x)=> {
+            action.canceled += (x) => {
                 var data = x.ReadValue<T>();
                 listener?.Invoke(false, data);
             };
         }
         else
-            Debug.Log("Input action can not Find Action:" + name);
+            Debug.Log("Input action cannot find Action: " + name);
     }
-    public void InputButtonBinding(string name,Action<bool> listener) 
+
+    // 用于绑定并监听按钮类型的输入（如点击）
+    public void InputButtonBinding(string name, Action<bool> listener)
     {
-        var action=InputSystem.actions.FindAction(name);
-        if ( action != null )
+        var action = InputSystem.actions.FindAction(name);
+        if (action != null)
         {
-            //action.started+= (x)=> {
-            //    listener?.Invoke(true);
-            //};
-            action.performed+= (x)=> {
+            action.performed += (x) => {
                 listener?.Invoke(true);
             };
 
-            action.canceled += (x)=> {
+            action.canceled += (x) => {
                 listener?.Invoke(false);
             };
         }
         else
-            Debug.Log("Input action can not Find Action:" + name);
+            Debug.Log("Input action cannot find Action: " + name);
     }
 
-    public InputManager()
-    {
-    }
+    public InputManager() { }
 }
--- Assets/_Scripts/MonoBehaviors/BigWorld/CWPlayer.cs
+++ Assets/_Scripts/MonoBehaviors/BigWorld/CWPlayer.cs
@@ -3,6 +3,10 @@ using UnityEngine;
 
 public class CWPlayer : Player
 {
+    private BagManager bagManager;  // 声明一个私有字段 bagManager
+
+    private float lastClickTime = 0f;  // 上一次点击的时间
+    private float doubleClickThreshold = 0.3f;  // 双击间隔的最大时间（单位：秒）
 
     Camera _cam;
     public override void OnInteractableTrigger(IPlayerInteractable interactable, bool isEnter)
@@ -18,6 +22,10 @@ public class CWPlayer : Player
     public void Init(Camera cam)
     {
         _cam = cam;
+
+        // 初始化玩家背包
+        Intent playerIntent = Intent.Get();
+        bagManager = new BagManager(playerIntent);
     }
 
     protected override void Awake()
@@ -29,7 +37,35 @@ public class CWPlayer : Player
                   {
                       OnMoveCLick();
                   }
+
               });
+        // 绑定鼠标左键点击事件，用于拾取物品
+        Manager<InputManager>.Inst.InputButtonBinding("MouseLeft", x =>
+        {
+            if (x)  // 按下鼠标左键时
+            {
+                TryPickUpItem();
+            }
+        });
+
+        // 绑定丢弃功能
+        Manager<InputManager>.Inst.InputButtonBinding(InputManager.DROP, x =>
+            {
+                if (x)  // 双击鼠标左键
+                {
+                    float timeSinceLastClick = Time.time - lastClickTime;
+
+                    // 如果两次点击之间的时间小于双击阈值，则视为双击
+                    if (timeSinceLastClick <= doubleClickThreshold)
+                    {
+                        DropItem();  // 执行丢弃物品操作
+                        Debug.Log("Double click detected. Item dropped.");
+                    }
+
+                    // 更新上一次点击的时间
+                    lastClickTime = Time.time;
+                }
+            });
     }
 
     void OnMoveCLick()
@@ -56,6 +92,56 @@ public class CWPlayer : Player
             }
         }
 
+    }
+
+    private void TryPickUpItem()
+    {
+        // 射线检测是否有物品可拾取
+        if (Input.mousePosition.x > 960)
+        {
+            // 获取鼠标在屏幕上的位置并调整为右半屏坐标
+            Vector3 localPos = Input.mousePosition - Vector3.right * 960f;
+
+            // 使用右侧相机从点击位置发射射线
+            Ray ray = _cam.ScreenPointToRay(localPos);
+
+            // 射线检测与3D物品的交互
+            if (Physics.Raycast(ray, out RaycastHit hit))
+            {
+                Debug.Log("yes");
+                if (hit.transform.tag == "Ground")
+                {
+                    Debug.Log("Move");
+                    _movement.SetFaceTargetPos(hit.point);
+                    _movement.SetFaceRotation(hit.point);
+                    _movement.SetFaceMoveTargetSpeed(MoveSpeed);
+                }
+                // 可被拾取的物品
+                if (hit.transform.tag == "PickupItem")
+                {
+                    GameObject item = hit.transform.gameObject;
+                    if (bagManager.AddItem(item))
+                    {
+                        Debug.Log($"{item.name} picked up!");
+                    }
+                    else
+                    {
+                        Debug.Log("Backpack is full!");
+                    }
+                }
+            }
+
+        }
+    }
+    private void DropItem()
+    {
+        // 从背包中移除物品并丢弃到玩家前方
+        GameObject item = bagManager.RemoveItem();
+        if (item != null)
+        {
+            item.transform.position = transform.position + transform.forward; // 丢弃在玩家前方
+            Debug.Log($"{item.name} dropped!");
+        }
     }
 
     protected override void Update()
